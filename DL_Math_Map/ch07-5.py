4fcf4c3df6c794a8c8a3f1cf8c2e3fc08bb3d0a9
PhlipTung <philip.dongfei@gmail.com>
2020-11-16 15:46:38 +0800

    DL Math Chapter 07, add 3 features, 4 features model

--- DL_Math_Map/ch07-5.py
+++ DL_Math_Map/ch07-5.py
@@ -16,11 +16,15 @@ print('feature name: ', feature_names)
 x_data = x_org[:,feature_names == 'RM']
 print('arranged:', x_data.shape)
 
+
 #insert x_0 = 1
 x = np.insert(x_data, 0, 1.0, axis=1)
 print('insert x_0: ', x.shape)
-print(x.shape)
-print(x[:5, : ])
+# insert LSTAT
+x_add = x_org[:, feature_names == 'LSTAT']
+x2 = np.hstack((x, x_add))
+print(x2.shape)
+print(x2[:5, : ])
 
 print(yt[:5])
 
@@ -35,18 +39,18 @@ def pred(x, w):
     return (x @ w)
 
 # initial process
-M = x.shape[0]
-D = x.shape[1]
-iters = 50000
-alpha = 0.01
+M = x2.shape[0]
+D = x2.shape[1]
+iters = 2000
+alpha = 0.001
 w = np.ones(D)
 history = np.zeros((0,2))
 
 # iterator compute
 for k in range(iters):
-    yp = pred(x, w)
+    yp = pred(x2, w)
     yd = yp - yt
-    w = w - alpha * (x.T @ yd) / M
+    w = w - alpha * (x2.T @ yd) / M
 
     if (k % 100 == 0):
         loss = np.mean(yd ** 2) / 2
@@ -57,17 +61,6 @@ print('Loss initial value: %f' % history[0,1])
 print('Loss final value: %f' % history[-1,1])
 print('w:\n', w)
 
-xall = x[:,1].ravel()
-x1 = np.array([[1, xall.min()],[1, xall.max()]])
-y1 = pred(x1, w)
-
-# distribute function
-plt.figure(figsize=(6,6))
-plt.scatter(x[:,1], yt, s=10, c='b')
-plt.xlabel('ROOM', fontsize=14)
-plt.ylabel('PRICE', fontsize=14)
-plt.plot(x1[:,1], y1, c='k')
-plt.show()
 
 # learning history
 plt.plot(history[1:, 0], history[1:,1])
